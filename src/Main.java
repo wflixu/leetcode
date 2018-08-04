60947b5789998c72f176c0d1ab2b1904579d1e27
wflixu <wflixu@yeah.net>
2018-08-04 23:30:31 +0800

    base02

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        Base1 base1 = new Base1();
-        int [] arr=  {2, 7, 11, 15};
-        int target=18;
-        int[] res= base1.twoSum(arr,target);
-        System.out.println("第一个结果："+res[0]);
-        System.out.println("第二个结果："+res[1]);
+//        Base1 base1 = new Base1();
+//        int [] arr=  {2, 7, 11, 15};
+//        int target=18;
+//        int[] res= base1.twoSum(arr,target);
+//        System.out.println("第一个结果："+res[0]);
+//        System.out.println("第二个结果："+res[1]);
+
+        Base2 base2 = new Base2();
+        int n =-123;
+        base2.reverse(n);
+
     }
 }
